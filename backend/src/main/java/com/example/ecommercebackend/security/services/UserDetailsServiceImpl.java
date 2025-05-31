d0e05a7c762aeee6a457462a0d34a19b08e14c00
marvindeckmyn <marvindeckmyn@gmail.com>
2025-05-31 19:50:34 +0200

    feat: Implement UserDetails and UserDetailsService for Spring Security

---
+++ backend/src/main/java/com/example/ecommercebackend/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.ecommercebackend.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.ecommercebackend.model.User;
+import com.example.ecommercebackend.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
