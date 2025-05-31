e35c231fc5cc716ce8ee26c28a7526b2eef76959
marvindeckmyn <marvindeckmyn@gmail.com>
2025-05-31 23:15:21 +0200

    Create AuthController

---
+++ backend/src/main/java/com/example/ecommercebackend/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.example.ecommercebackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ecommercebackend.payload.request.LoginRequest;
+import com.example.ecommercebackend.payload.request.SignupRequest;
+import com.example.ecommercebackend.service.AuthService;
+
+import jakarta.validation.Valid;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        return authService.authenticateUser(loginRequest);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
+        return authService.registerUser(signupRequest);
+    }
+}
