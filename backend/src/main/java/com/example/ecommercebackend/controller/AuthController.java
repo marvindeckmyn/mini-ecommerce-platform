f476696ab5962c3b942db785036b6f5f862baec2
marvindeckmyn <marvindeckmyn@gmail.com>
2025-06-01 14:15:47 +0200

    Fix: resolve some registration issues

--- backend/src/main/java/com/example/ecommercebackend/controller/AuthController.java
+++ backend/src/main/java/com/example/ecommercebackend/controller/AuthController.java
@@ -2,7 +2,6 @@ package com.example.ecommercebackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +13,6 @@ import com.example.ecommercebackend.service.AuthService;
 
 import jakarta.validation.Valid;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
